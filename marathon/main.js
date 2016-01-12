33d231b89789fe5fdca1da7eb8ba6045c44cedc0
Raul Tammesalu <tammesalu@gmail.com>
2016-01-12 18:42:34 +0200

    Added main.js

---
+++ marathon/main.js
@@ -0,0 +1,17 @@
+/*
+ * main.js
+ *
+ * Created by Raul Tammesalu 12 January 2016.
+ */
+
+/*global
+  $, console
+*/
+
+function main() {
+  'use strict';
+
+  console.log('Marathon App.');
+}
+
+$(document).ready(main);
